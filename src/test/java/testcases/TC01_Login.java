49c4629ee6bde60e5cf11aafcd9254f08b602e49
bob1995 <Bob1995>
2025-03-29 20:18:08 +0530

    Added Data driven code

--- src/test/java/testcases/TC01_Login.java
+++ src/test/java/testcases/TC01_Login.java
@@ -1,5 +1,14 @@
 package testcases;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.helpers.Reporter;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageobjects.LoginPageObjects;
@@ -8,13 +17,38 @@ public class TC01_Login extends Setup {
 
 	LoginPageObjects loginPageobjects;
 
-	@Test
-	public void verifyLogin() {
-		loginPageobjects=new LoginPageObjects(webdriver);
-		loginPageobjects.enterUsername("student");
-		loginPageobjects.enterPassword("Password123");
+	@Test(dataProvider = "DataDriven")
+	public void verifyLogin(String userName, String password) {
+		loginPageobjects = new LoginPageObjects(webdriver);
+		System.out.println("UserName:"+userName);
+		System.out.println("Password:"+password);
+		loginPageobjects.enterUsername(userName);
+		loginPageobjects.enterPassword(password);
 		loginPageobjects.clickOnLoginButton();
 
+	}
+
+	@DataProvider(name = "DataDriven")
+	public Object[][] testDataGenerator() throws Exception {
+		FileInputStream fileInputStream = new FileInputStream(
+				System.getProperty("user.dir") + "//src//test//resources//TestData//Credentials.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+		// Get first/desired sheet from the workbook
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		int numberofRows = sheet.getPhysicalNumberOfRows();
+		
+		Object[][] testData = new Object[numberofRows][2];
+
+		for (int i = 0; i < numberofRows; i++) {
+			Reporter.info("Number of rows"+numberofRows);
+			XSSFRow row = sheet.getRow(i);
+			XSSFCell userName = row.getCell(0);
+			XSSFCell password = row.getCell(1);
+			testData[i][0] = userName.getStringCellValue();
+			testData[i][1] = password.getStringCellValue();
+
+		}
+		return testData;
 	}
 
 }
