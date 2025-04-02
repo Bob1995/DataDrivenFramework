49387bc4ff8ed98e1651b78d23386853c9f46ac6
bob1995 <Bob1995>
2025-04-02 22:36:05 +0530

    Added Before method annotation

--- src/test/java/testcases/Setup.java
+++ src/test/java/testcases/Setup.java
@@ -5,27 +5,28 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 import Utilities.PropertiesReader;
 
 public class Setup {
 
 	public static WebDriver webdriver;
 
-	@SuppressWarnings("deprecation")
-	@BeforeTest
+	@BeforeMethod
 	public void navigateToURL() {
 		webdriver = new ChromeDriver();
 		PropertiesReader.readProperties("URL.properties");
 		webdriver.get(PropertiesReader.properties.getProperty("baseURL"));
-		Reporter.log("Launching Brower:"+ PropertiesReader.properties.getProperty("baseURL"));
+		Reporter.log("Launching Brower:" + PropertiesReader.properties.getProperty("baseURL"));
 		webdriver.manage().window().maximize();
-		webdriver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		webdriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 	}
 
-	@AfterTest
+	
+	@AfterMethod
 	public void closeBrowser() {
 		webdriver.quit();
 
